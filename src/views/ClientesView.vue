aaef39f42c785347aadd7268ebd14b20bdc24645
Daniel151505 <daniel150520011@gmail.com>
2023-10-10 23:48:41 -0500

    Creando el Header

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,9 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div>
-        <h1>Clientes</h1>
-    </div>
+  <div>
+    <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
+  </div>
 </template>
