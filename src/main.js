437c60c6a36d772087ea5f4984454e26e99e3808
Daniel151505 <daniel150520011@gmail.com>
2023-10-12 22:12:43 -0500

    Añadiendo un tema para el CSS de Formkit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { plugin, defaulConfig } from '@formkit/vue'
+import config from '../formkit.config'
 import App from './App.vue'
 import router from './router'
 
@@ -8,6 +9,6 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(router)
-app.use(plugin, defaulConfig())
+app.use(plugin, defaulConfig(config))
 
 app.mount('#app')
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -19,17 +19,26 @@ defineProps({
     <HeadingVue>{{ titulo }}</HeadingVue>
 
     <div class="mx-auto mt-10 bg-white shadow">
-      <FormKit type="form">
-        <FormKit
-          type="text"
-          label="Nombre"
-          placeholder="Nombre de Cliente"
-          help="Coloca el Nombre del cliente que desear registrar"
-          validation="required"
-          :validation-messages="{ required: 'El nombre del cliente es obligatorio'}"
-          validation-visibility="live"
-        />
-      </FormKit>
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit type="form">
+          <FormKit
+            type="text"
+            label="Nombre"
+            placeholder="Nombre de Cliente"
+            help="Coloca el Nombre del cliente que desear registrar"
+            validation="required"
+            :validation-messages="{
+              required: 'El nombre del cliente es obligatorio',
+            }"
+          />
+        </FormKit>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.formkit-wrapper {
+  max-width: 100%;
+}
+</style>
